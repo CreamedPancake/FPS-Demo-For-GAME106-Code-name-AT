3ca5409c50e3687501cc87fc42dbba8b7ab1dd67
CreamedPancake <80748628+CreamedPancake@users.noreply.github.com>
2022-03-26 23:01:05 +0300

    Additions as instructor told

--- AimTrain/106_fps_demo_Aim_Training_Cem_aydin_kocabas_116207068/Assets/Pistol/animate.cs
+++ AimTrain/106_fps_demo_Aim_Training_Cem_aydin_kocabas_116207068/Assets/Pistol/animate.cs
@@ -6,6 +6,8 @@ public class animate : MonoBehaviour
 {
     public GameObject M9;
     public bool isfiring = false;
+    public AudioSource PistolShot;
+    public GameObject MuzzleFlash; 
     void Update()
     {
             if (Input.GetMouseButtonDown(0))
@@ -20,7 +22,10 @@ public class animate : MonoBehaviour
     {
         isfiring = true;
         M9.GetComponent<Animator>().Play("Fire");
+        PistolShot.Play();
+        MuzzleFlash.SetActive(true);
         yield return new WaitForSeconds(0.10f);
+        MuzzleFlash.SetActive(false);
         M9.GetComponent<Animator>().Play("New State");
         isfiring = false;
     }
